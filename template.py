def24304c301623b7df82538af3dcac0cc8c9ac1
Michael <38026865+michaelcollis@users.noreply.github.com>
2018-05-15 14:49:42 +1000

    Update template.py
    made a new branch

--- template.py
+++ template.py
@@ -3,10 +3,10 @@ A longer description of the module with details that may help the user or anybod
 reviewing the code later. make sure you outline in detail what the module does and how it can be used.
 """
 
-__author__ = "Your Name"
+__author__ = "Michael, Lochie and Matthew"
 __copyright__ = "Copyright 2018, NSW Dept. Ed."
 __license__ = "GPL"
 __version__ = "1.0.1"
-__maintainer__ = "Your Name again unless you are cheating"
-__email__ = "your.addressk@education.nsw.com.au"
+__maintainer__ = "Michael"
+__email__ = "michael.collis2@education.nsw.gov.au"
 __status__ = "Prototype, Development or Production"
